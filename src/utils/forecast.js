b0301b37cff4ff376ca02c41fdc598551655da63
Sehyung Lee <sehyunglee@Sehyung-ui-MacBook-Pro.local>
2020-02-16 15:38:39 +0900

    Add high to forecast

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -2,6 +2,11 @@
 
 const request = require('request')
 
+
+
+
+
+
 const forecast = (latitude, longitude, callback) => {
     const url = 'https://api.darksky.net/forecast/41e35ba31aaec5a4f21ac3a5414711be/' + latitude + ',' + longitude
 
@@ -11,7 +16,7 @@ const forecast = (latitude, longitude, callback) => {
         } else if (body.error) {
             callback('Unable to find location', undefined)
         } else {
-            callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degrees out. There is a ' + body.currently.precipProbability + '% chance of rain.')
+            callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degrees out. There is a ' + body.currently.precipProbability + '% chance of rain.' + 'The high Today is ' + body.daily.data[0].temperatureHigh)
         }
     })
 }
